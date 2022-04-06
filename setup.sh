bb31a238cea9003ab20cf09be437140d0a956a89
lucciffer <cipherr01@protonmail.com>
2022-04-06 19:03:05 +0530

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+wget https://exiftool.org/Image-ExifTool-12.40.tar.gz
+gzip -dc Image-ExifTool-12.40.tar.gz | tar -xf -
+cd Image-ExifTool-12.40
+perl Makefile.PL
+make test
+sudo make install
+pip install pyexiftool
+sudo apt install libzbar0
+pip install zbar pysolar 
+pip install requests packaging pytest-xdist pyzbar mapboxgl jenkspy rawpy imageio gdal nb_conda
